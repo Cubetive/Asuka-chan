109b0c9d663232055f0456422efdae523dd88afd
Cubetive <peternguyenstudent123@gmail.com>
2024-04-13 17:11:09 -0400

    adding listeners and stuff ig

--- src/main/java/com/fubukigrin/AsukaChan.java
+++ src/main/java/com/fubukigrin/AsukaChan.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class AsukaChan {
-    private static final String token = "MTIyODc5ODYwNTg4NDc4NDczMg.GydoZV.9Df6bwrPLY3AnoaW3bWaacgNHbdB4wT6bgjxrU";
+    protected static final String token = "MTIyODc5ODYwNTg4NDc4NDczMg.GydoZV.9Df6bwrPLY3AnoaW3bWaacgNHbdB4wT6bgjxrU";
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = JDABuilder.createDefault(token);
@@ -16,7 +16,7 @@ public class AsukaChan {
         builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
         // Enable the bulk delete event
         builder.setBulkDeleteSplittingEnabled(false);
-        // Set activity (like "playing Something")
+        // Setting activity
         builder.setActivity(Activity.watching("fubukiGrin"));
         
         builder.build();

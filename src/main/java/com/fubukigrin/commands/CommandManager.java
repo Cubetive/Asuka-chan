98b01397b33b60f20d7e28feb049924a77bec5e0
Cubetive <peternguyenstudent123@gmail.com>
2024-04-14 07:45:13 -0400

    unimportant

--- src/main/java/com/fubukigrin/commands/CommandManager.java
+++ src/main/java/com/fubukigrin/commands/CommandManager.java
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 public class CommandManager extends ListenerAdapter {
     @Override

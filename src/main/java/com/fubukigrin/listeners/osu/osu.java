f1fd31a8304e2eaab8c48b2792cd120b00ca805b
Vietng <vietng322611@gmail.com>
2024-04-14 12:12:21 +0700

    Initialize

---
+++ src/main/java/com/fubukigrin/listeners/osu/osu.java
@@ -0,0 +1,16 @@
+package com.fubukigrin.listeners.osu;
+
+import javax.annotation.Nonnull;
+import java.net.*;
+
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+@SuppressWarnings("unused")
+
+public class osu extends ListenerAdapter {
+    public osu() {}
+}

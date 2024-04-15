95a77c68c5a919626865f3a7e6d9fad548c89272
Vietng <vietng322611@gmail.com>
2024-04-15 16:31:58 +0700

    Add getUserData

--- src/main/java/com/fubukigrin/commands/osu/osu.java
+++ src/main/java/com/fubukigrin/commands/osu/osu.java
@@ -1,7 +1,14 @@
 package com.fubukigrin.commands.osu;
 
-import javax.annotation.Nonnull;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
 import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+import org.apache.commons.lang3.math.*;
 
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.User;
@@ -12,5 +19,36 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 @SuppressWarnings("unused")
 
 public class osu extends ListenerAdapter {
-    public osu() {}
+    private static String BaseUrl = "https://osu.ppy.sh/api/v2/";
+    private static String beatmap = "beatmap";
+    private static String scores = "beatmap/scores";
+    private static String users = "users";
+
+    Endpoints api;
+
+    public osu() throws Exception {
+        api = new Endpoints();
+    }
+
+    public JsonNode getUserData(String user) throws Exception {
+        String key;
+        if (NumberUtils.isNumber(user)) { key = "id"; }
+        else { key = "username"; }
+
+        String temp = BaseUrl + users + "/" + user + "/osu?key=" + key;
+        URI uri = new URI(temp.replace(" ", "%20"));
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+            .uri(uri)
+            .header("Accept", "application/json")
+            .header("Content-Type", "application/json")
+            .header("Authorization", String.format("Bearer %s", api.accessToken))
+            .GET()
+            .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> response = httpClient.send(postRequest, BodyHandlers.ofString());
+
+        return null;
+    }
 }

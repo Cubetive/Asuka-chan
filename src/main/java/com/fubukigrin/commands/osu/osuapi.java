3e10af94b354eb1599d57045cbddaaedeeedeaa0
Cubetive <peternguyenstudent123@gmail.com>
2024-04-15 14:38:15 -0400

    Rename files according to java naming conventions. Changed config.json

--- src/main/java/com/fubukigrin/commands/osu/osuapi.java
+++ src/main/java/com/fubukigrin/commands/osu/osuapi.java
@@ -18,7 +18,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 @SuppressWarnings("unused")
 
-public class osuapi extends ListenerAdapter {
+public class OsuAPI extends ListenerAdapter {
     private static String BaseUrl = "https://osu.ppy.sh/api/v2/";
     private static String beatmap = "beatmaps/";
     private static String users = "users/";
@@ -27,7 +27,7 @@ public class osuapi extends ListenerAdapter {
 
     Endpoints api;
 
-    public osuapi() throws Exception {
+    public OsuAPI() throws Exception {
         api = new Endpoints();
         objectMapper = new ObjectMapper();
     }

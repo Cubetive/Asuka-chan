21b7d4867c5f947c8749405fcd6ccf5f7465a3b4
Cubetive <peternguyenstudent123@gmail.com>
2024-04-17 16:45:53 -0400

    :3c

--- src/main/java/com/fubukigrin/commands/InvalidCommandArgumentException.java
+++ src/main/java/com/fubukigrin/commands/InvalidCommandArgumentException.java
@@ -4,7 +4,7 @@ package com.fubukigrin.commands;
  * This exception class is mainly for the bot to return 
  * funny messages and stuff along the line lol. Maybe it can 
  * be implemented later? :3c
-**/
+ */
 public class InvalidCommandArgumentException extends Exception {
     private String errorMessage;
 

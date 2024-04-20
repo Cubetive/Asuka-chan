c0b46a88f2704ea4c6a3a656a60e6d3643b81939
Cubetive <peternguyenstudent123@gmail.com>
2024-04-20 16:34:34 -0400

    Main part of osu command EmbedBuilder completed

--- src/main/java/com/fubukigrin/commands/osu/Osu.java
+++ src/main/java/com/fubukigrin/commands/osu/Osu.java
@@ -1,11 +1,15 @@
 package com.fubukigrin.commands.osu;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import com.fubukigrin.commands.InvalidCommandArgumentException;
 import com.fubukigrin.utilities.ConvertDateTime;
+import com.fubukigrin.utilities.OsuGrades;
 import com.osu.OsuAPI;
 import com.osu.UserData;
 
@@ -16,6 +20,9 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 
 public class Osu {
+    private static String ONLINE = "https://cubetive.s-ul.eu/kLWl06Lw";
+    private static String OFFLINE = "https://cubetive.s-ul.eu/pEQn8tB0";
+
     @SuppressWarnings("null")
     public static void execute(@Nonnull SlashCommandInteractionEvent event) {
         event.deferReply().queue();
@@ -52,7 +59,7 @@ public class Osu {
             event.getChannel().sendMessage(MessageCreateData.fromEmbeds(embed)).queue();
         } 
         catch (Exception e) {
-            String error = (user != null) ? String.format("User %s was not found", user) : "Invalid arguments! Please try again";
+            String error = (user != null) ? String.format("User `%s` was not found", user) : "Invalid arguments! Please try again";
 
             InvalidCommandArgumentException ica = new InvalidCommandArgumentException(error);
             String errorMessage = ica.getErrorMessage();
@@ -61,13 +68,16 @@ public class Osu {
     }
 
     public static EmbedBuilder buildEmbed(UserData userData) {
+        System.out.println(userData.lastSeen);
+
         EmbedBuilder eb = new EmbedBuilder();
         eb.setColor(new Color(195, 98, 110));
 
         // Title
         String title = String.format("%s: %,.2fpp (#%,d %s%,d)", userData.username, userData.pp, userData.globalRank, userData.countryCode, userData.countryRank);
         String profileLink = "https://osu.ppy.sh/users/" + userData.id + "/osu";
-        eb.setAuthor(title, profileLink);
+        String iconURL = "https://assets.ppy.sh/old-flags/" + userData.countryCode + ".png";
+        eb.setAuthor(title, profileLink, iconURL);
 
         // Avatar and flag
         eb.setThumbnail(userData.avatarUrl.toString());
@@ -75,17 +85,30 @@ public class Osu {
         // Body
         int playTimeHours = userData.playTime / 3600;
         double currentLevel = userData.levelCurrent + (userData.levelProgress / 100.0);
+        
+        List<Integer> grades = new ArrayList<Integer>();
+        userData.gradeCounts.forEach((grade) -> { grades.add(grade.asInt()); });
+        Collections.swap(grades, 0, 1); // Swap XH and X
+        Collections.swap(grades, 2, 3); // Swap SH and S
+
+        String rankBuilder = String.format("**▸ Ranks:** %s`pl`%s`pl`%s`pl`%s`pl`%s`pl`%n", OsuGrades.XH, OsuGrades.X, OsuGrades.SH, OsuGrades.S, OsuGrades.A);
+        rankBuilder = rankBuilder.replace("pl", "%,d"); // Replacing placeholder with integer format
 
         StringBuffer body = new StringBuffer();
-        body.append(String.format("**▸ Peak rank:** `%,d` achieved %s%n", userData.peakRank, ConvertDateTime.toRelativeDiscordTimestamp(userData.peakRankUpdate)))
-            .append(String.format("**▸ Accuracy:** `%,.2f%s` • **Level**: `%s`%n", userData.accuracy, "%" ,currentLevel))
+        body.append(String.format("**▸ Peak rank:** `#%,d` achieved %s%n", userData.peakRank, ConvertDateTime.toRelativeDiscordTimestamp(userData.peakRankUpdate)))
+            .append(String.format("**▸ Accuracy:** `%,.2f%s` • **Level:** `%s`%n", userData.accuracy, "%" ,currentLevel))
             .append(String.format("**▸ Playcount:** `%,d` (`%d hrs`)%n", userData.playCount, playTimeHours))
+            .append(String.format("**▸ Medals:** `%d` • **Badges:** `%d`%n", userData.achievements.size(), userData.badges.size()))
+            .append(String.format(rankBuilder, grades.toArray()))
             .append(String.format("**▸ Join date:** `%s` (%s)", ConvertDateTime.toShortDate(userData.joinDate), ConvertDateTime.toRelativeDiscordTimestamp(userData.joinDate)));
 
         eb.setDescription(body.toString());
 
         // Footer (nothing right now)
-        // String footer = "";
+        String footer = (userData.isOnline) ? "Currently online on osu!Bancho" : String.format("Last seen %s on osu! Bancho", ConvertDateTime.toRelativeTime(userData.lastSeen));
+        String status = (userData.isOnline) ? ONLINE : OFFLINE;
+
+        eb.setFooter(footer, status);
 
         return eb;
     }
--- src/main/java/com/fubukigrin/utilities/ConvertDateTime.java
+++ src/main/java/com/fubukigrin/utilities/ConvertDateTime.java
@@ -6,8 +6,8 @@ import java.time.format.DateTimeFormatter;
 
 public class ConvertDateTime {
     public static long getEpochSecond(String timestamp) {
-        DateTimeFormatter f = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        Instant parseTime = Instant.from(f.parse(timestamp));
+        DateTimeFormatter osuFormat = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        Instant parseTime = Instant.from(osuFormat.parse(timestamp));
         return parseTime.getEpochSecond();
     }
 
@@ -20,8 +20,58 @@ public class ConvertDateTime {
     }
 
     public static String toShortDate(String timestamp) {
-        DateTimeFormatter f = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-        LocalDate parseDate = LocalDate.parse(timestamp, f);
+        DateTimeFormatter osuFormat = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        LocalDate parseDate = LocalDate.parse(timestamp, osuFormat);
         return parseDate.toString();
+    }
+
+    public static String toLongDateTime(String timestamp) {
+        DateTimeFormatter osuFormat = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        DateTimeFormatter longDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        Instant parseTime = Instant.from(osuFormat.parse(timestamp));
+        String parseDate = longDateTimeFormat.format(parseTime);
+        return parseDate;
+    }
+
+    public static String toRelativeTime(String timestamp) {
+        DateTimeFormatter osuFormat = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        Instant now = Instant.now();
+        Instant parseTime = Instant.from(osuFormat.parse(timestamp));
+
+        long timeInBetween = now.getEpochSecond() - parseTime.getEpochSecond();
+
+        String relative = new String();
+
+        if (timeInBetween < 60) {
+            // Seconds
+            relative = (timeInBetween > 1) ? timeInBetween + " seconds ago" : "1 second ago";
+        } 
+        else if (timeInBetween < 3600) {
+            // Minutes
+            timeInBetween /= 60;
+            relative = (timeInBetween > 1) ? timeInBetween + " minutes ago" : "1 minute ago";
+        }
+        else if (timeInBetween < 86400) {
+            // Hours
+            timeInBetween /= 3600;
+            relative = (timeInBetween > 1) ? timeInBetween + " hours ago" : "1 hour ago";
+        }
+        else if (timeInBetween < 5184000) {
+            // Days
+            timeInBetween /= 86400;
+            relative = (timeInBetween > 1) ? timeInBetween + " days ago" : "1 day ago";
+        }
+        else if (timeInBetween < 31557600) {
+            // Months
+            timeInBetween /= 2592000;
+            relative = timeInBetween + " months ago";
+        }
+        else {
+            // Years (to accomendate leap years, I treat it as 365.25 days)
+            timeInBetween /= 31557600;
+            relative = (timeInBetween > 1) ? timeInBetween + " days ago" : "1 year ago";
+        }
+
+        return relative;
     }
 }
